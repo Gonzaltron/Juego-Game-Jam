2421fd883025ef59fb5b8923f486d1d5c0bff388
Gorka Urrutia del Pico <gorka_urrutiadelpico@euneiz.com>
2025-11-29 12:06:20 +0100

    wip: GameManager logic

--- Juego Jam/Assets/Source/GameManager/GameManager.cs
+++ Juego Jam/Assets/Source/GameManager/GameManager.cs
@@ -1,7 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.LogError("Game manager is NULL");
+                instance = new GameManager();
+                return instance;
+            }
+            else
+            {
+                return instance;
+            }
+        }
+    }
+
+    private GameManager()
+    {
+
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
