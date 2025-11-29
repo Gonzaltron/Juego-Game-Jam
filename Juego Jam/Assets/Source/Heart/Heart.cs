640d6d6eda5a189e534fa7086b3ecfca5906a548
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2025-11-29 10:09:21 +0100

    Heart partially done (still to integrate with the imput manager)

--- Juego Jam/Assets/Source/Heart/Heart.cs
+++ Juego Jam/Assets/Source/Heart/Heart.cs
@@ -7,28 +7,47 @@ public class Heart : MonoBehaviour
     [SerializeField]int maxtimer;
     int timer;
     [SerializeField]int quickTime;
+    [SerializeField] TMPro.TextMeshProUGUI debugText;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(Timer());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            StopAllCoroutines();
+            StartCoroutine(Timer());
+        }
     }
 
     IEnumerator Timer()
     {
         timer = Random.Range(mintimer, maxtimer +1);
-        yield return new WaitForSeconds(timer);
+        for(int i = timer; i <= timer&& i>0; i--)
+        {
+            yield return new WaitForSeconds(1);
+            Debug.Log("Tiempo: " + i);
+            debugText.text = "Tiempo: " + i;
+        }
         StartCoroutine(QuickTime());
     }
 
     IEnumerator QuickTime()
     {
-        yield return new WaitForSeconds(quickTime);
+        for(int i = quickTime; i <= quickTime && i>0; i--)
+        {
+            Debug.Log("Presiona E! " + i);
+            debugText.text = "Presiona E! " + i;
+            yield return new WaitForSeconds(1);
+        }
+        Debug.Log("Game Over");
+        
+        
         //se apaga la pantalla
     }
 }
