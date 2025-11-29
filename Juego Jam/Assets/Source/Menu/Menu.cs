0bfc240f84d8bbf447f503598ffd0240922a6f05
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2025-11-29 18:00:18 +0100

    Menu canvas tutorial added

--- Juego Jam/Assets/Source/Menu/Menu.cs
+++ Juego Jam/Assets/Source/Menu/Menu.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] Canvas tutorial;
+    [SerializeField] Canvas MenuCanvas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        MenuCanvas = GameObject.Find("MenuCanvas").GetComponent<Canvas>();
+        MenuCanvas.enabled = true;
+        tutorial = GameObject.Find("TutorialCanvas").GetComponent<Canvas>();
+        tutorial.enabled = false;
     }
 
     // Update is called once per frame
@@ -22,5 +27,17 @@ public class Menu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void OpenTutorial()
+    {
+        MenuCanvas.enabled = false;
+        tutorial.enabled = true;
+    }
+
+    public void CloseTutorial()
+    {
+        tutorial.enabled = false;
+        MenuCanvas.enabled = true;
     }
 }
